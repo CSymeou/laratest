4b81aca7c4208439346eee57b8b4e69d4a314bf0
CSymeou <c.symeou@blupath.co.uk>
2020-12-12 21:44:52 +0200

    Defined routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,76 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Dashboard
 Route::get('/', function () {
+    return view('welcome');
+});
+
+// Tasks
+Route::get('/tasks/create', function () {
+    return view('welcome');
+});
+
+Route::post('/tasks', function () {
+    return view('welcome');
+});
+
+Route::get('/tasks/{task}/edit', function () {
+    return view('welcome');
+});
+
+Route::patch('/tasks/{task}/edit', function () {
+    return view('welcome');
+});
+
+Route::delete('/tasks/{task}', function () {
+    return view('welcome');
+});
+
+Route::get('/users/create', function () {
+    return view('welcome');
+});
+
+// Users
+Route::get('/users/create', function () {
+    return view('welcome');
+});
+
+Route::post('/users', function () {
+    return view('welcome');
+});
+
+Route::get('/users/{user}/edit', function () {
+    return view('welcome');
+});
+
+Route::patch('/users/{task}/edit', function () {
+    return view('welcome');
+});
+
+Route::delete('/users/{task}', function () {
+    return view('welcome');
+});
+
+// My Tasks
+Route::get('/mytasks', function () {
+    return view('welcome');
+});
+
+Route::get('/mytasks/{task}/edit', function () {
+    return view('welcome');
+});
+
+Route::patch('/tasks/{task}/edit', function () {
+    return view('welcome');
+});
+
+// Team tasks
+Route::get('/teamtasks', function () {
+    return view('welcome');
+});
+
+// Team leaders
+Route::get('/teamleaders', function () {
     return view('welcome');
 });
